b2f30af64aa69bd58e849de52e41a70cf125320c
Romi Halasz <novocainex@gmail.com>
2017-05-13 17:53:58 +0300

    Menu component.

--- app/sections/AbstractMenuSection.php
+++ app/sections/AbstractMenuSection.php
@@ -4,6 +4,8 @@ abstract class AbstractMenuSection extends AbstractSection {
 	const LOGIN_SECTION = 'login';
 	const LOGOUT_SECTION = 'logout';
 
+	private $requiresLogin = array('apps', 'clients', 'logout');
+
 	protected function init($userIsLoggedIn = false) {
 		$this->initMenu($this->appFacade->getSections(), $userIsLoggedIn);
 	}
@@ -15,7 +17,11 @@ abstract class AbstractMenuSection extends AbstractSection {
 	private function parseMenuItems($menuItems, $loggedIn) {
 
 		foreach($menuItems as $index => $item)
-			if(($loggedIn && $item->name == self::LOGIN_SECTION) || (!$loggedIn && $item->name == self::LOGOUT_SECTION))
+			if(
+				($loggedIn && $item->name == self::LOGIN_SECTION) ||
+				(!$loggedIn && array_search($item->name, $this->requiresLogin) !== false)
+				)
+				
 				unset($menuItems[$index]);
 
 		return $menuItems;
