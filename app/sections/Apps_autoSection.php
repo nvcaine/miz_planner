57d0f02c09e517ce7f55eba82088772fa42b09e1
Romi Halasz <novocainex@gmail.com>
2017-05-27 16:41:03 +0300

    Small cleanup.

--- app/sections/Apps_autoSection.php
+++ app/sections/Apps_autoSection.php
@@ -12,22 +12,5 @@ class Apps_autoSection extends AbstractSection {
 		$db = DBWrapper::cloneInstance();
 		$query = "SELECT * FROM clients WHERE last_name LIKE '$query%' OR first_name LIKE '$query%' ORDER BY last_name";
 		return $db->query($query);
-		/*$clients = json_decode(file_get_contents('json/clients.json'));
-		$result = array();
-
-		foreach($clients as $client)
-			if(
-				strpos(strtolower($client->first_name), strtolower($query)) !== false ||
-				strpos(strtolower($client->last_name), strtolower($query)) !== false ||
-				strpos(strtolower($client->first_name . ' ' . $client->last_name), strtolower($query)) !== false
-				)
-				$result[] = $client;
-
-		usort($result, array($this, "clientCompare"));
-		return $result;*/
-	}
-
-	private function clientCompare($c1, $c2) {
-		return strcmp($c1->last_name, $c2->last_name);
 	}
 }
